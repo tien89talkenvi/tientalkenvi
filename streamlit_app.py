1c9aecaaab87f83d7a6a321afe7bdb2e9658d111
tien89talkenvi <147873351+tien89talkenvi@users.noreply.github.com>
2025-08-04 00:14:31 -0500

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -275,11 +275,10 @@ def ThucThiPhan_1():
         st.write('Nếu không đạt, có thể đến trực tiếp trang sau để tải:')
         st.markdown("1-[Open Page SMARTS](https://smarts.waterboards.ca.gov/smarts/SwPublicUserMenu.xhtml)", unsafe_allow_html=True)
         st.write('2-Click on “Public User Menu”')
-        st.write('3-Click on “Public User Menu”')
-        st.write('4-Click on “Download NOI Data By Regional Board”')
-        st.write('5-Select your region from the dropdown menu')
-        st.write('6-Click on both “Industrial Application Specific Data” and “Industrial Ad Hoc Reports - Parameter Data”')
-        st.write('7-Data will be downloaded to two separate .txt files, each titled “file”')
+        st.write('3-Click on “Download NOI Data By Regional Board”')
+        st.write('4-Select your region from the dropdown menu')
+        st.write('5-Click on both “Industrial Application Specific Data” and “Industrial Ad Hoc Reports - Parameter Data”')
+        st.write('6-Data will be downloaded to two separate .txt files, each titled “file”')
 #========================= MAIN =====================================================================
 # TIEU DE APP
 st.header('🏷️Trình hỗ trợ quản lý môi trường nước')
